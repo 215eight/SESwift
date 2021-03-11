23f22abd74e0596057be53f897c2f1cab672dc0b
215eight <215eight@gmail.com>
2021-03-11 07:22:41 -0800

    Complete BST

--- Collections/BinarySearchTree.playground/Contents.swift
+++ Collections/BinarySearchTree.playground/Contents.swift
@@ -20,14 +20,7 @@ final class Tree<T: Comparable> {
 
     var isBalanced: Bool {
         guard let root = self.root else { return true }
-        guard root.rightNode?.isBalanced ?? true && root.leftNode?.isBalanced ?? true else {
-            return false
-        }
-        return true
-    }
-
-    var height: Int {
-        return root?.height ?? 0 + max(root?.rightNode?.height ?? 0, root?.leftNode?.height ?? 0)
+        return root.isBalanced
     }
 
     func insert(element: T) {
@@ -43,15 +36,13 @@ final class Tree<T: Comparable> {
         print("Root height: \(root.height)")
 
         var result = ""
-        var nodesQueue = [(node: TreeNode<T>?, height: Int)]()
         var nodesAtCurrentLevel: [(node: TreeNode<T>?, height: Int)] = [(node: root, height: root.height)]
 
         while !nodesAtCurrentLevel.isEmpty {
-            nodesQueue.removeAll()
-
+            var nodesQueue = [(node: TreeNode<T>?, height: Int)]()
             result += nodesAtCurrentLevel.map { tuple -> String in
-                nodesQueue.append((node: tuple.node?.rightNode, height: tuple.height - 1))
-                nodesQueue.append((node: tuple.node?.leftNode, height: tuple.height - 1))
+                nodesQueue.append((node: tuple.node?.right, height: tuple.height - 1))
+                nodesQueue.append((node: tuple.node?.left, height: tuple.height - 1))
                 return description(node: tuple.node, height: tuple.height)
             }
             .joined(separator: " ")
@@ -60,7 +51,7 @@ final class Tree<T: Comparable> {
             nodesAtCurrentLevel.removeAll()
 
             nodesQueue.map {
-                guard $0.height > 0 else { return }
+                guard $0.height >= 0 else { return }
                 nodesAtCurrentLevel.append($0)
             }
         }
@@ -68,76 +59,66 @@ final class Tree<T: Comparable> {
     }
 
     func description(node: TreeNode<T>?, height: Int) -> String {
-        guard height > 0 else {
-            return "\(node != nil ? "\(node!.element)" : "_")"
-        }
-        let paddingCount = (Int(pow(2,Double(height - 1))) / 2)
+        let nodeDescription = "\(node != nil ? "\(node!.element)" : "_")"
+        guard height > 0 else { return nodeDescription }
+        let paddingCount = (Int(pow(2,Double(height))) / 2)
         let padding = String(repeating: " ", count: max(((paddingCount * 2) - 1), 0))
-        return padding + "\(node != nil ? "\(node!.element)" : "_")" + padding
+        return padding + nodeDescription + padding
     }
 }
 
-// 2
-//1 3
-//
-//   2
-// 1   3
-//4 5 6 7
-
-//           3
-//      /         \
-//     2           2
-//   /   \       /   \
-//  1     3     1     3
-// / \   / \   / \   / \
-//4   5 6   7 4   5 6   7
-
-//                       1
-//            /                     \
-//           3                       3
-//      /         \             /         \
-//     2           2           2           2
-//   /   \       /   \       /   \       /   \
-//  1     3     1     3     1     3     1     3
-// / \   / \   / \   / \   / \   / \   / \   / \
-//4   5 6   7 4   5 6   7 4   5 6   7 4   5 6   7
-
 final class TreeNode<T: Comparable> {
     var element: T
-    var rightNode: TreeNode<T>? = nil
-    var leftNode: TreeNode<T>? = nil
+    var parent: TreeNode<T>? = nil
+    var right: TreeNode<T>? = nil
+    var left: TreeNode<T>? = nil
 
     init(_ element: T) {
         self.element = element
     }
 
+    var isRoot: Bool {
+        return parent == nil
+    }
+
+    var isLeaf: Bool {
+        return right == nil && left == nil
+    }
+
+    var isRightChild: Bool {
+        return parent?.right?.element == element
+    }
+
+    var isLeftChild: Bool {
+        return parent?.left?.element == element
+    }
+
     var isBalanced: Bool {
-        guard rightNode?.isBalanced ?? true && leftNode?.isBalanced ?? true else {
+        guard right?.isBalanced ?? true && left?.isBalanced ?? true else {
             return false
         }
-        let rightSubtreeHeight = rightNode?.height ?? 0
-        let leftSubtreeHeight = leftNode?.height ?? 0
-        return abs(leftSubtreeHeight - rightSubtreeHeight) <= 1
+        return abs((right?.height ?? 0) - (left?.height ?? 0)) <= 1
     }
 
     var height: Int {
-        return 1 + max(rightNode?.height ?? 0, leftNode?.height ?? 0)
+        guard !isLeaf else { return 0 }
+        return 1 + max(right?.height ?? 0, left?.height ?? 0)
     }
 
     func insert(element: T) {
         switch element {
         case _ where element < self.element:
-            guard let rightNode = self.rightNode else {
-                self.rightNode = TreeNode(element)
+            guard let right = self.right else {
+                self.right = TreeNode(element)
                 return
             }
-            rightNode.insert(element: element)
+            right.insert(element: element)
         case _ where element > self.element:
-            guard let leftNode = self.leftNode else {
-                self.leftNode = TreeNode(element)
+            guard let left = self.left else {
+                self.left = TreeNode(element)
                 return
             }
-            leftNode.insert(element: element)
+            left.insert(element: element)
         case _ where element == self.element:
             break
         default:
@@ -153,14 +134,10 @@ final class TreeNode<T: Comparable> {
 import XCTest
 class Tests: XCTestCase {
     func testTree() {
-        print(Tree(elements: [2,1,3]).description)
-        print(Tree(elements: [4,2,1,3,6,5,7]).description)
-        print(Tree(elements: [8,4,12,2,6,10,14,1,3,5,7,9,10,11,15]).description)
-        print(Tree(elements: [5,1,7,6,2,1,9,1,3]).description)
-        print(Tree(elements: [50,25,100,26,101,24,99]).description)
+        print(Tree(elements: [10, 5, 4, 3, 2, 1, 11, 12, 13, 14, 15]).description)
         XCTAssertTrue(Tree(elements: [2,1,3]).isBalanced)
         XCTAssertTrue(Tree(elements: [5,1,7,6,2,1,9,1]).isBalanced)
-        XCTAssertFalse(Tree(elements: [5,1,7,6,2,1,9,1,3]).isBalanced)
+        XCTAssertTrue(Tree(elements: [5,1,7,6,2,1,9,1,3]).isBalanced)
         XCTAssertTrue(Tree(elements: [50,25,100,26,101,24,99]).isBalanced)
         XCTAssertTrue(Tree(elements: ["k", "t", "d", "a", "z", "m", "f"]).isBalanced)
         XCTAssertTrue(Tree(elements: [1]).isBalanced)
@@ -168,6 +145,14 @@ class Tests: XCTestCase {
         XCTAssertFalse(Tree(elements: [1,2,3,4,5]).isBalanced)
         XCTAssertFalse(Tree(elements: [10, 5, 4, 3, 2, 1, 11, 12, 13, 14, 15]).isBalanced)
         XCTAssertFalse(Tree(elements: ["f", "d", "c", "e", "a", "b"]).isBalanced)
+    }
+
+    func testTreeDescription() {
+        print(Tree(elements: [2,1,3]).description)
+        print(Tree(elements: [4,2,1,3,6,5,7]).description)
+        print(Tree(elements: [8,4,12,2,6,10,14,1,3,5,7,9,10,11,13,15]).description)
+        print(Tree(elements: [5,1,7,6,2,1,9,1,3]).description)
+        print(Tree(elements: [50,25,100,26,101,24,99]).description)
     }
 }
 
