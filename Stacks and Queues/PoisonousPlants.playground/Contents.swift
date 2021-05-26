deeaf01cdd09d47c39190111b988889e71e2f7a6
215eight <215eight@gmail.com>
2021-05-25 22:43:28 -0700

    Poisonous Plants 3

--- Stacks and Queues/PoisonousPlants.playground/Contents.swift
+++ Stacks and Queues/PoisonousPlants.playground/Contents.swift
@@ -8,6 +8,48 @@
 
 func poisonousPlants(p: [Int]) -> Int {
 
+    var plants = p
+    guard let firstPlant = plants.first else {
+        return 0
+    }
+    plants.removeFirst()
+
+    var lastLevel = 0
+    var plantsQueue = [firstPlant]
+
+    for plant in plants {
+        print(plantsQueue)
+        var comparisonLevel = lastLevel
+        while comparisonLevel >= 0 {
+            let plantComparison = plantsQueue[comparisonLevel]
+            if plant > plantComparison {
+                let newIndex = lastLevel - comparisonLevel + 1
+                if (0..<plantsQueue.count).contains(newIndex) {
+                    plantsQueue[newIndex] = plant
+                } else {
+                    plantsQueue.append(plant)
+                }
+                print(lastLevel, comparisonLevel)
+                if newIndex < plantsQueue.count - 1 {
+                    lastLevel = plantsQueue.count - 1
+                } else {
+                    lastLevel = newIndex
+                }
+                break
+            }
+            comparisonLevel -= 1
+        }
+        if comparisonLevel == -1 {
+            plantsQueue[0] = plant
+            lastLevel = 0
+        }
+    }
+
+    return plantsQueue.count - 1
+}
+
+func poisonousPlants1(p: [Int]) -> Int {
+
     func filter(plants: [Int], count: Int) -> (plants: [Int], count: Int) {
         guard plants.count > 1 else {
             return (plants, count)
@@ -39,20 +81,13 @@ func poisonousPlants(p: [Int]) -> Int {
 import XCTest
 class Tests: XCTestCase {
     func testPoisonousPlants() {
-        XCTAssertEqual(poisonousPlants(p: [1,1,1,1,1,1,1]), 0)
-        XCTAssertEqual(poisonousPlants(p: [3,6,2,7,5]), 2)
-        XCTAssertEqual(poisonousPlants(p: [6,5,8,4,7,10,9]), 2)
-        XCTAssertEqual(poisonousPlants(p: [3,7,1,2,4,8,2,7,10]), 2)
-        XCTAssertEqual(poisonousPlants(p: [4,3,7,5,6,4,2]), 3)
-//        4 3 7 5 6 4 2
-//        4 3 5 4 2
-//        4 3 4 2
-//        4 3 2
-        XCTAssertEqual(poisonousPlants(p: [3,1,10,7,3,5,6,6]), 3)
-//        3,1,10,7,3,5,6,6
-//        3,1,7,3,6,6
-//        3,1,3,6
-//        3,1
+//        XCTAssertEqual(poisonousPlants(p: [1,1,1,1,1,1,1]), 0)
+//        XCTAssertEqual(poisonousPlants(p: [3,6,2,7,5]), 2)
+//        XCTAssertEqual(poisonousPlants(p: [6,5,8,4,7,10,9]), 2)
+//        XCTAssertEqual(poisonousPlants(p: [3,7,1,2,4,8,2,7,10]), 2)
+//        XCTAssertEqual(poisonousPlants(p: [4,3,7,5,6,4,2]), 3)
+//        XCTAssertEqual(poisonousPlants(p: [3,1,10,7,3,5,6,6]), 3)
+        XCTAssertEqual(poisonousPlants(p: [11,7,19,6,12,12,8,8,7,1,10,15,5,12]), 5)
     }
 }
 
