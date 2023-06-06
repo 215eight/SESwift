08fea05cf6c7c7ebcdfd12acb83e93f2b9b0b597
215eight <215eight@gmail.com>
2023-06-05 18:34:47 -0700

    Update Balanced Brackets

--- Algorithms/BalancedBrackets.playground/Contents.swift
+++ Algorithms/BalancedBrackets.playground/Contents.swift
@@ -30,6 +30,60 @@ func balancedBrackets(_ input: String) -> Bool {
     return queue.isEmpty
 }
 
+extension Character {
+    var isOpenBracket: Bool {
+        let characterSet = CharacterSet(charactersIn: "([{<")
+        guard let scalar = self.unicodeScalars.first else { return false }
+        return characterSet.contains(scalar)
+    }
+
+    var isCloseBracket: Bool {
+        let characterSet = CharacterSet(charactersIn: ")]}>")
+        guard let scalar = self.unicodeScalars.first else { return false }
+        return characterSet.contains(scalar)
+    }
+
+    func isMatchingCloseBracket(_ bracket: Character) -> Bool {
+        let bracketsMap: [Character: Character] = [
+            "(" : ")",
+            "[" : "]",
+            "{" : "}",
+            "<" : ">",
+        ]
+
+        guard let matchingCloseBaracket = bracketsMap[self] else { return false }
+        return bracket == matchingCloseBaracket
+    }
+}
+
+func balancedBracketsRecursive(openInput: String, input: String) -> Bool {
+    guard let firstChar = input.first else {
+        return openInput.isEmpty
+    }
+
+    var updatedInput = input
+    updatedInput.removeFirst()
+
+    if firstChar.isOpenBracket {
+        let updatedOpenInput = openInput + "\(firstChar)"
+        return balancedBracketsRecursive(openInput: updatedOpenInput, input: updatedInput)
+    } else if firstChar.isCloseBracket {
+        guard let lastOpenBracket = openInput.last,
+              lastOpenBracket.isMatchingCloseBracket(firstChar) else {
+            return false
+        }
+        var updatedOpenInput = openInput
+        updatedOpenInput.removeLast()
+        return balancedBracketsRecursive(openInput: updatedOpenInput, input: updatedInput)
+    } else {
+        return false
+    }
+}
+
+func balancedBrackets2(_ input: String) -> Bool {
+    return balancedBracketsRecursive(openInput: "", input: input)
+}
+
 /*:
  # Tests
  */
@@ -48,6 +102,62 @@ class Tests: XCTestCase {
         XCTAssertFalse(balancedBrackets("(["))
         XCTAssertFalse(balancedBrackets("[<<<{}>>]"))
         XCTAssertFalse(balancedBrackets("hello"))
+    }
+
+    func testIsOpenBracket() {
+        let openParenthesis = Character("(")
+        XCTAssertTrue(openParenthesis.isOpenBracket)
+        let openBracket = Character("[")
+        XCTAssertTrue(openBracket.isOpenBracket)
+        let openCurlyBrace = Character("{")
+        XCTAssertTrue(openCurlyBrace.isOpenBracket)
+        let openAngleBracket = Character("<")
+        XCTAssertTrue(openAngleBracket.isOpenBracket)
+        let randomChar = Character("a")
+        XCTAssertFalse(randomChar.isOpenBracket)
+    }
+
+    func testIsMatchingCloseBracket() {
+        let openParenthesis = Character("(")
+        XCTAssertTrue(openParenthesis.isMatchingCloseBracket(")"))
+        XCTAssertFalse(openParenthesis.isMatchingCloseBracket("a"))
+        let openBracket = Character("[")
+        XCTAssertTrue(openBracket.isMatchingCloseBracket("]"))
+        XCTAssertFalse(openBracket.isMatchingCloseBracket(")"))
+        let openCurlyBrace = Character("{")
+        XCTAssertTrue(openCurlyBrace.isMatchingCloseBracket("}"))
+        XCTAssertFalse(openCurlyBrace.isMatchingCloseBracket("-"))
+        let openAngleBracket = Character("<")
+        XCTAssertTrue(openAngleBracket.isMatchingCloseBracket(">"))
+        XCTAssertFalse(openAngleBracket.isMatchingCloseBracket("}"))
+    }
+
+    func testBalancedBrackets2() {
+        XCTAssertTrue(balancedBrackets2("()"))
+        XCTAssertTrue(balancedBrackets2("([])"))
+        XCTAssertTrue(balancedBrackets2("([])(<{}>)"))
+        XCTAssertTrue(balancedBrackets2("([]{}<[{}]>)"))
+        XCTAssertTrue(balancedBrackets2(""))
+        XCTAssertFalse(balancedBrackets2("}{"))
+        XCTAssertFalse(balancedBrackets2("([)]"))
+        XCTAssertFalse(balancedBrackets2("([)"))
+        XCTAssertFalse(balancedBrackets2("(["))
+        XCTAssertFalse(balancedBrackets2("[<<<{}>>]"))
+        XCTAssertFalse(balancedBrackets2("hello"))
+    }
+
+    func testBalanceBracketsPerformance() {
+        let openBrackets = String(repeating: "(", count: 100)
+        let closeBrackets = String(repeating: ")", count: 100)
+        let testInput = openBrackets + closeBrackets
+        XCTAssertTrue(balancedBrackets(testInput))
+    }
+
+    func testBalanceBrackets2Performance() {
+        let openBrackets = String(repeating: "(", count: 100)
+        let closeBrackets = String(repeating: ")", count: 100)
+        let testInput = openBrackets + closeBrackets
+        XCTAssertTrue(balancedBrackets2(testInput))
     }
 }
 
